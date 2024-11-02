dea5a578ceae87547eaea3abd35021f3f2f08171
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-03 03:09:40 +0600

    setting the carousel only , but not set in bg dynamically

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
         maxWidth: {
           'custom-container': '1140px',
         },
+        perspective: {
+          'none': 'none',
+          'sm': '500px',
+          'md': '1000px',
+          'lg': '1500px',
+          'xl': '2000px',
+        },
       },
     },
     plugins: [],
